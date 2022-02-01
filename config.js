8b8f5ae12a93f9d14d577923f131a807c51b867f
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-02-01 20:49:27 +0100

    do stuff

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports.token = "OTM3NzQ4Mjc1OTMzNjMwNTM0.YfgQIw.rWFNbkqtbCiUcNoseTuAZPj8zIA"
 module.exports.clientid = "937748275933630534"
-module.exports.test = "test"
+module.exports.test = "test2"
